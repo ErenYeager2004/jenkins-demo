a61362ed31eb58ff4fd14975b72f7b17267bd17a
Debjit <debjitsaha606@gmail.com>
2026-09-24 16:09:14 +0000

    Initial Spring Boot application

---
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.jenkinsdemo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class JenkinsDemoApplication {
+   public static void main(String[] args) {
+       SpringApplication.run(JenkinsDemoApplication.class, args);
+   }
+   @GetMapping("/")
+   public String home() {
+       return "Hello from Jenkins CI/CD!";
+   }
+}
